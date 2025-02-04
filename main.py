d42206e976bf2193f62b8e52cdca33aa901c225d
SAWKIT <potions.plaquiste0l@icloud.com>
2025-02-05 00:57:16 +0100

    Bot mention Detection

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ TOKEN = os.getenv("DISCORD_SECRET_CLIENT")
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
@@ -19,13 +20,17 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
+    if message.content.startswith(f"<@{bot.user.id}>"):
+        print(f"{message.author} me parle !")
+
     print(f"Message de {message.author}: {message.content}")
 
     await bot.process_commands(message)
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Salut !")
+@bot.event
+async def on_member_join(member):
+    welcome_channel: discord.TextChannel = bot.get_channel("933379075995758603")
+    await welcome_channel.send(f"Bienvenue {member.display_name} !")
 
 if TOKEN:
     bot.run(TOKEN)
