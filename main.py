57e70a82d0ea5c3e1ed6fa242260399988b5b67b
SAWKIT <potions.plaquiste0l@icloud.com>
2025-02-05 02:32:20 +0100

    Welcome Messages Added

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import discord
+import discord, os, random
 from discord.ext import commands
-import os
 
 
-TOKEN = os.getenv("DISCORD_SECRET_CLIENT")
+DISCORD_TOKEN = os.getenv("DISCORD_SECRET_CLIENT")
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
+
 
 @bot.event
 async def on_ready():
@@ -20,7 +20,7 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    if message.content.startswith(f"<@{bot.user.id}>"):
+    if bot.user.mentioned_in(message):
         print(f"{message.author} me parle !")
 
     print(f"Message de {message.author}: {message.content}")
@@ -30,9 +30,23 @@ async def on_message(message):
 @bot.event
 async def on_member_join(member):
     welcome_channel: discord.TextChannel = bot.get_channel("933379075995758603")
-    await welcome_channel.send(f"Bienvenue {member.display_name} !")
 
-if TOKEN:
-    bot.run(TOKEN)
+    messages = [
+        f"Bienvenue {member.display_name} ! 😬",
+        f"Est-ce un oiseau ? Un avion ?! Mais non, c'est {member.display_name} !",
+        f"Saint pétard ti, vla {member.display_name} ! On est foutus...",
+        f"Si on m'avait dit que je verrai {member.display_name} un jour... 😏",
+        f"{member.display_name} vient d'entrer dans l'arène ! Espérons qu'il survive 🤞",
+        f"Oh non... Pas encore un {member.display_name}... On en avait pas déjà un en stock ? 😆",
+        f"La légende disait vrai... {member.display_name} existe vraiment 👀",
+        f"{member.display_name} a été invoqué avec succès !",
+        f"On pensait être tranquilles... et voilà que {member.display_name} arrive 😬",
+    ]
+
+    await welcome_channel.send(random.choice(messages))
+
+
+if DISCORD_TOKEN:
+    bot.run(DISCORD_TOKEN)
 else:
     print("ERROR : DISCORD_SECRET_CLIENT - Le token du bot Discord est manquant.")
