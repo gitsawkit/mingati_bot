7eb3dc40e5dfd829bf5f0e124cb17719027181c3
SAWKIT <potions.plaquiste0l@icloud.com>
2025-02-06 01:54:37 +0100

    Create Channel Feature

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ DISCORD_TOKEN = os.getenv("DISCORD_SECRET_CLIENT")
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
+intents.voice_states = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
@@ -41,7 +42,7 @@ async def on_message(message):
 
 @bot.event
 async def on_member_join(member):
-    welcome_channel: discord.TextChannel = bot.get_channel("933379075995758603")
+    welcome_channel: discord.TextChannel = bot.get_channel(933379075995758603)
 
     messages = [
         f"Bienvenue {member.display_name} ! 😬",
@@ -56,6 +57,68 @@ async def on_member_join(member):
     ]
 
     await welcome_channel.send(random.choice(messages))
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    if after.channel and after.channel.name == "➕・CRÉER UN SALON" and after.channel.category.name.startswith(("↽🎮・Gaming", "↽💬・Forum")):
+        await create_channel(member)
+    if before.channel and before.channel.name.startswith(f"{member.display_name}'s Palace") and len(before.channel.members) == 0:
+            await before.channel.delete()
+
+async def create_channel(member):
+    guild = member.guild
+    overwrites = {
+        member: discord.PermissionOverwrite(
+            # General Permissions
+            view_channel = True,
+            manage_channels = True,
+            manage_permissions = True,
+            manage_webhooks = True,
+            # Members Permissions
+            create_instant_invite = True,
+            # Voice Channels Permissions
+            connect = True,
+            speak = True,
+            stream = True,
+            use_soundboard = True,
+            use_external_sounds = True,
+            use_voice_activation = True,
+            priority_speaker = True,
+            mute_members = True,
+            deafen_members = True,
+            move_members = True,
+            # Chat Voice Channels Permissions
+            send_messages = True,
+            embed_links = True,
+            attach_files = True,
+            add_reactions = True,
+            use_external_emojis = True,
+            use_external_stickers = True,
+            ## USE SON EXTERNE
+            mention_everyone = True,
+            manage_messages = True,
+            read_message_history = True,
+            send_tts_messages = True,
+            send_voice_messages = True,
+            create_polls = True,
+            # Events Permissions
+            create_events = True,
+            manage_events = True,
+            # Applications Permissions
+            use_application_commands = True,
+            use_embedded_activities = True,
+            use_external_apps = True,
+        )
+    }
+
+    category = member.voice.channel.category
+    if category:
+        new_channel = await guild.create_voice_channel(
+            name=f"{member.display_name}'s Palace",
+            category=category,
+            overwrites=overwrites,
+        )
+        await member.move_to(new_channel)
 
 
 if DISCORD_TOKEN:
